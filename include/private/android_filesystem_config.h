305c466594022697c2fa5e2ced352e5f02ab7126
Daniel Micay <danielmicay@gmail.com>
2015-04-16 19:10:08 -0400

    add proc group for use with hidepid

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -98,6 +98,8 @@
 #define AID_NET_BW_ACCT   3007  /* change bandwidth statistics accounting */
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
 
+#define AID_PROC          3009  /* access to processes of other users */
+
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
@@ -181,6 +183,8 @@ static const struct android_id_info android_ids[] = {
     { "net_bw_stats",  AID_NET_BW_STATS, },
     { "net_bw_acct",   AID_NET_BW_ACCT, },
     { "net_bt_stack",  AID_NET_BT_STACK, },
+
+    { "proc",          AID_PROC, },
 
     { "everybody",     AID_EVERYBODY, },
     { "misc",          AID_MISC, },
