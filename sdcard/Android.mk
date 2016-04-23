913e0783cfeb7748943835fe650e527ac294dbf3
Daniel Micay <danielmicay@gmail.com>
2016-04-23 15:48:29 -0400

    sdcard: enable integer and object-size sanitizers
    Change-Id: I9e402a4c327ae9695c368a36cb1fb56e39a5006f

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -6,6 +6,9 @@ LOCAL_SRC_FILES := sdcard.c
 LOCAL_MODULE := sdcard
 LOCAL_CFLAGS := -Wall -Wno-unused-parameter -Werror
 
+LOCAL_CFLAGS += -fno-wrapv -fsanitize=integer,object-size -fsanitize-undefined-trap-on-error
+LOCAL_CLANG := true
+
 LOCAL_SHARED_LIBRARIES := libcutils
 
 include $(BUILD_EXECUTABLE)
