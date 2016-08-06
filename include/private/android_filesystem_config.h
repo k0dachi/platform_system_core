5e7d1f4a1975a73d3de366726f1ac335eec1c361
Daniel Micay <danielmicay@gmail.com>
2016-08-05 21:41:56 -0400

    remove shared relro support
    Change-Id: Iba2f7b446e7707e9cd71079b4d2eb868649fad78

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -76,7 +76,6 @@
 #define AID_SDCARD_AV     1034  /* external storage audio/video access */
 #define AID_SDCARD_ALL    1035  /* access all users external storage */
 #define AID_LOGD          1036  /* log daemon */
-#define AID_SHARED_RELRO  1037  /* creator of shared GNU RELRO files */
 
 #define AID_SHELL         2000  /* adb and debug shell user */
 #define AID_CACHE         2001  /* cache access */
@@ -173,7 +172,6 @@ static const struct android_id_info android_ids[] = {
     { "sdcard_av",     AID_SDCARD_AV, },
     { "sdcard_all",    AID_SDCARD_ALL, },
     { "logd",          AID_LOGD, },
-    { "shared_relro",  AID_SHARED_RELRO, },
 
     { "shell",         AID_SHELL, },
     { "cache",         AID_CACHE, },
